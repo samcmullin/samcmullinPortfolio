4b5f5df451a5a6e1f86225df3a1c855aca487865
Stephen McMullin <stephen.mcmullin@icloud.com>
2020-08-27 01:30:59 -0400

    styles updated and responsiveness added

---
+++ assets/scripts/script.js
@@ -0,0 +1,12 @@
+$(function() {
+    
+    // Smooth scrolling after link click
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+    
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1000);
+    });
+})
+
